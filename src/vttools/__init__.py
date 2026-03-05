f2fd5097070eb9adad5c542373a654bb42bd8c63
Erik Domogalla <erikdomogalla@gmail.com>
2026-03-05 16:54:55 +0100

    fix Ordnerstruktur

--- src/vttools/__init__.py
+++ src/vttools/__init__.py
@@ -1,5 +1,5 @@
 """vttools: wiederverwendbare Utilities für Auswertung und Engineering-Workflows."""
 
-from .tables import df_to_table
+from vttools.Data.tables import df_to_table
 
 __all__ = ["df_to_table"]
